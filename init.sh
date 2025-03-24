5bd1d5bae36591627328226c37aedf84e7f70960
Leonardo Araújo <ledharaujo@gmail.com>
2025-03-24 14:28:27 -0300

    Adiciona portainer para gestão do cluster.

--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@
 
 docker swarm init --advertise-addr <SEU_IP>
 docker network create --driver=overlay traefik-public
+
 docker stack deploy -c traefik.yml traefik
 docker stack deploy -c wallos-stack.yml wallos
+docker stack deploy -c portainer.yml portainer
